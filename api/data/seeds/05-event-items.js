2da8dc37dd8f6142159aa74376fdcf0a71186892
AD9018 <adamdadi23@gmail.com>
2021-06-25 13:31:42 -0400

    WIP

--- api/data/seeds/05-event-items.js
+++ api/data/seeds/05-event-items.js
@@ -2,7 +2,7 @@ exports.seed = function (knex) {
   return knex("event_items").insert([
     {
       event_item_id: 1,
-      quantity: 3,
+      quantity: 2,
       item_id: 1,
       event_id: 1
     },
